310ff419d4b57dd46a0f4cdec04021dbb292a0c3
TheDiabolicalKid <marine.opsommer@gmail.com>
2026-01-26 12:13:11 +0100

    fix header tsx
    Co-authored-by: manuloup <manuloup@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
+import Galerie from "./pages/Galerie";
+import Reservation from "./pages/Reservation";
 import "./App.css";
 
 function App() {
@@ -12,7 +14,11 @@ function App() {
 			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/menu" element={<Menu />} />
+				<Route path="/Menu" element={<Menu />} />
+				<Route path="/Galerie" element={<Galerie />} />
+				<Route path="/Reservation" element={<Reservation />} />
+
+
 			</Routes>
 			<Footer />
 		</BrowserRouter>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
 function Header() {
-	return (
-		<header className="header">
-			<Link to="/">Home</Link>
-			<Link to="/menu">Menu</Link>
-		</header>
-	);
+return(
+    <header className="header">
+        <nav className="header__nav">
+            <NavLink to="/"className="header__link">
+            Accueil
+                </NavLink>
+                <NavLink to="menu"className="header__link">
+            Les Menus>
+                </NavLink>
+                        <NavLink to="/galerie" className="header__link">
+          Galerie
+        </NavLink>
+
+        <NavLink to="/reservation" className="header__link">
+          Réservations
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
-export default Header;
