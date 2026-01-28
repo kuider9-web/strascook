a6efcff9e9399419bc442f45f4d54e4f914f4ec9
TheDiabolicalKid <marine.opsommer@gmail.com>
2026-01-28 16:37:20 +0100

    start galerie tsx and css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,13 @@ import "./Header.css";
 function Header() {
 	return (
 		<header className="header">
-			<Link to="/">Home</Link>
-			<Link to="/menu">Menu</Link>
-			<Link to="/galerie">Galerie</Link>
-			<Link to="/reservation">Réservation</Link>
+			
+			<nav className="header-nav">
+				<Link to="/">Home</Link>
+				<Link to="/menu">Menu</Link>
+				<Link to="/galerie">Galerie</Link>
+				<Link to="/reservation">Réservation</Link>
+			</nav>
 		</header>
 	);
 }
