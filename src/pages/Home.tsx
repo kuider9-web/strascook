f6242c49ca9f1a1d92b3478e63628139c23dd269
unknown <kuider9@gmail.com>
2026-01-29 14:32:28 +0100

    debug fichier

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
-import "./Home.css";
-
 function Home() {
-	return <div />;
+	return (
+		<div>
+			<h1>Gastronomique</h1>
+			<p>Une cuisine d'auteur, local et innovante.</p>
+		</div>
+	);
 }
 
 export default Home;
