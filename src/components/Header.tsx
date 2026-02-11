a73b12ee17279816eecb2df60306a19850313bcf
manuloup <galland.manuloup@gmail.com>
2026-02-11 14:34:06 +0100

    Fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
+import logo from "../asset/image/logo1.png";
 
 function Header() {
+	// Fonction pour remonter en haut de la page
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+	};
+
 	return (
 		<header className="header">
 			<video className="header-video" autoPlay loop muted playsInline>
@@ -10,11 +19,20 @@ function Header() {
 			</video>
 			<div className="header-overlay"></div>
 
+			{/* Navbar sticky */}
 			<nav className="header-nav">
-				<Link to="/">Accueil</Link>
-				<Link to="/menu">Les Menus</Link>
-				<Link to="/galerie">Galerie</Link>
-				<Link to="/reservation">Réservations</Link>
+				{/* Logo cliquable qui remonte */}
+				<button onClick={scrollToTop} className="header-logo" type="button">
+					<img src={logo} alt="Gastronomique Logo" />
+				</button>
+
+				{/* Navigation au centre */}
+				<div className="header-nav-links">
+					<Link to="/">Accueil</Link>
+					<Link to="/menu">Les Menus</Link>
+					<Link to="/galerie">Galerie</Link>
+					<Link to="/reservation">Réservations</Link>
+				</div>
 			</nav>
 
 			<div className="header-content">
