917d9c262b6cb69e35d2264c5b5dfd7fbacbcbff
kuider9-web <kuider9@gmail.com>
2026-02-11 10:44:18 +0100

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-
 import { PanierProvider } from "./context/PanierContext";
-
 import BackOffice from "./pages/BackOffice/BackOffice";
-
 import Galerie from "./pages/Galerie";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
@@ -15,7 +11,6 @@ import Reservation from "./pages/Reservation";
 
 function App() {
 	return (
-
 		<PanierProvider>
 			<BrowserRouter>
 				<Header />
@@ -24,23 +19,11 @@ function App() {
 					<Route path="/menu" element={<Menu />} />
 					<Route path="/galerie" element={<Galerie />} />
 					<Route path="/reservation" element={<Reservation />} />
+					<Route path="/admin" element={<BackOffice />} />
 				</Routes>
 				<Footer />
 			</BrowserRouter>
 		</PanierProvider>
-
-		<BrowserRouter>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/menu" element={<Menu />} />
-				<Route path="/galerie" element={<Galerie />} />
-				<Route path="/reservation" element={<Reservation />} />
-				<Route path="/admin" element={<BackOffice />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
-
 	);
 }
 
